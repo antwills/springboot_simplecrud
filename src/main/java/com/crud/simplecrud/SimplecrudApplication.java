b01638b265544c412ceec53879d5ac39de5c6a87
AntWills <antoniowillissilvasantos@gmail.com>
2025-11-20 10:22:39 -0300

    Teste no linux

--- src/main/java/com/crud/simplecrud/SimplecrudApplication.java
+++ src/main/java/com/crud/simplecrud/SimplecrudApplication.java
@@ -1,7 +1,10 @@
 package com.crud.simplecrud;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class SimplecrudApplication {
